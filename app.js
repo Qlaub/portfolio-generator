b203e4d5d7ce4ffce202a58115536cda713f310e
Alex Glaubitz <e.pirazzi@gmail.com>
2022-04-12 15:38:10 -0400

    template generates projects and about section

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage =  require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', pageHTML, err => {
-//   if (err) throw new Error(err); 
-
-//   console.log('Portfolio complete! Check out index.html to see the output!')
-// })
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -76,7 +68,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: userInput => {
+        if (userInput) {
+          return true;
+        } else {
+          console.log('Please enter the name of your project')
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -136,5 +136,12 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData)
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('index.html', pageHTML, err => {
+      if (err) throw new Error(err); 
+
+      console.log('Portfolio complete! Check out index.html to see the output!')
+    })
   })
+
